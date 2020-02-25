db130caf7ce3164cde83d4494c03d853b9842d86
Parker Franzi <Parker.franzi@gmail.com>
2020-02-24 16:36:49 -0800

    connected again

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://shrouded-hollows-87789.herokuapp.com/api',
     TOKEN_KEY: 'family-recipes-auth-token',
     CLOUDINARY_URL: 'https://res.cloudinary.com/djvgcsmhq/image/upload'
 }
