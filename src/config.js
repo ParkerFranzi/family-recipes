dcb7f0187d3a53ca7edd0ea7e5dac54690b408b9
Parker Franzi <Parker.franzi@gmail.com>
2020-04-07 18:06:06 -0700

    forgot the api call

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'https://shrouded-hollows-87789.herokuapp.com',
+    API_ENDPOINT: 'https://shrouded-hollows-87789.herokuapp.com/api',
     API_ENDPOINT_LOCAL: 'http://localhost:8000/api',
     TOKEN_KEY: 'family-recipes-auth-token',
     CLOUDINARY_URL: 'https://res.cloudinary.com/djvgcsmhq/image/upload'
