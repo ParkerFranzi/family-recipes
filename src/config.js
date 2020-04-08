bf464de5576c86767eabf8c5aa6348499ca8d919
Parker Franzi <Parker.franzi@gmail.com>
2020-04-07 17:28:03 -0700

    added production endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://shrouded-hollows-87789.herokuapp.com/',
+    API_ENDPOINT_LOCAL: 'http://localhost:8000/api',
     TOKEN_KEY: 'family-recipes-auth-token',
     CLOUDINARY_URL: 'https://res.cloudinary.com/djvgcsmhq/image/upload'
 }
