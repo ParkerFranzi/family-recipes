980ec1412ad876ab93e0e228287075036caa376c
Parker Franzi <Parker.franzi@gmail.com>
2020-06-20 12:59:22 -0700

    missed a picture to update

--- src/components/User/UserList.js
+++ src/components/User/UserList.js
@@ -9,7 +9,7 @@ export default class UserList extends Component {
             <NavLink to={`/users/${this.props.id}`}>
                 <div className="user-box">
                     <div className="user-img">
-                        <img alt={this.props.fname + "-profile-pic"} src={`${config.CLOUDINARY_URL}/w_250,h_250,q_auto/${this.props.public_id}.${this.props.pic_type}`} />
+                        <img alt={this.props.fname + "-profile-pic"} src={`${config.CLOUDINARY_URL}/w_250,h_250,q_auto,c_thumb/${this.props.public_id}.${this.props.pic_type}`} />
                     </div>
                     <div className="user-name">
                         <h3>{this.props.fname + " " + this.props.lname}</h3>
